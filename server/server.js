d46326440bee4683908e6252128cd2c07bcdad76
Gaurav Thakur <gauravt@channel-fusion.com>
2026-02-07 14:41:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 4000;
+
+// Serve React
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });

aa19d217d2f315530cbf5362c043044cdd0672e8
Gaurav Thakur <gauravt@channel-fusion.com>
2026-02-07 15:04:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [status, setStatus] = useState("");
 
   useEffect(() => {
-    api.get("http://localhost:5000/health").then(res => setStatus(res.data.status));
+    api.get("https://jokes-e0qo.onrender.com/health").then(res => setStatus(res.data.status));
   }, []);
 
   return <h1>Backend status: {status}</h1>;
